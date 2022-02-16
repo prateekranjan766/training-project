f9b02be763c46021059abf30e49e2d221035efc0
Prateek Ranjan <pranjan@tekion.com>
2022-02-17 02:12:57 +0530

    Dummy data stored in model in script.js and dynamic rendering of content is implemented from the sidebar menu.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 (function swiggyClone() {
   //Model
   model = {
+    activeMenuIndex: 0,
+    actions: {
+      CHANGE_ACTIVE_MENU: "CHANGE_ACTIVE_MENU",
+    },
     sidebarList: [
       "recommended",
       "platters",
@@ -40,6 +44,99 @@
           imgLocation: "./images/dishes/non-veg-platter.jpeg",
         },
       ],
+      platters: [
+        {
+          isVeg: false,
+          name: "Mixed Platter",
+          price: 799,
+          details:
+            "An authentic mixed platter with 3 pieces of Barnala Methi Chicken, 3 pieces of Chicken Kebab, 3 pieces of Hara Bhara and 3 pieces of Paneer Tikka.",
+          imgLocation: "./images/dishes/mixed-platter.jpeg",
+        },
+        {
+          isVeg: true,
+          name: "Veg Platter",
+          price: 699,
+          details:
+            "An authentic veg platter with 3 pieces of Paneer Achari, 3 pieces of Hara Bhara, 3 pieces of Veg Seekh and 3 pieces of Malai Chaap.",
+          imgLocation: "./images/dishes/veg-platter.jpeg",
+        },
+      ],
+      starters: [
+        {
+          isVeg: true,
+          name: "Paneer Malai Tikka",
+          price: 239,
+          details: "Slow-cooked Paneer Tikka mildly flavoured with rich cream.",
+          imgLocation: "./images/dishes/paneer-malai-tikka.jpeg",
+        },
+        {
+          isVeg: false,
+          name: "Dhaniya Chicken Tikka",
+          price: 249,
+          details:
+            "Succulent chicken marinated & flavoured with fresh coriander, grilled to perfection.",
+          imgLocation: "./images/dishes/dhaniya-chicken-tikka.jpeg",
+        },
+      ],
+      "family binge packs": [
+        {
+          isVeg: false,
+          name: "Dhaniya Chicken Tikka",
+          price: 249,
+          details:
+            "Succulent chicken marinated & flavoured with fresh coriander, grilled to perfection.",
+          imgLocation: "./images/dishes/dhaniya-chicken-tikka.jpeg",
+        },
+      ],
+      "main course": [
+        {
+          isVeg: false,
+          name: "Butter Chicken",
+          price: 249,
+          details:
+            "A classic chicken dish prepared in a rich sauce of tomato, butter and cream.",
+          imgLocation: "./images/dishes/butter-chicken.jpeg",
+        },
+      ],
+      "rice & indian breads": [
+        {
+          isVeg: true,
+          name: "Naan",
+          price: 40,
+          details: "A leavened flatbread cooked in a tandoor (clay oven).",
+          imgLocation: "./images/dishes/naan.jpeg",
+        },
+      ],
+      "premium thalis": [
+        {
+          isVeg: false,
+          name: "Premium Butter Chicken Roti Thali",
+          price: 289,
+          details:
+            "Butter Chicken , 2 nos roti served with pickle , raita ,gulab jamun, 2 Pcs Chicken Tikka",
+          imgLocation: "./images/dishes/premium-butter-chicken-roti-thali.jpeg",
+        },
+      ],
+      "deserts & beverages": [
+        {
+          isVeg: true,
+          name: "Gulab Jamun",
+          price: 99,
+          details:
+            "Sweet and sumptuous dough balls soaked in cinnamon flavoured sugar syrup.",
+          imgLocation: "./images/dishes/gulab-jamun.jpeg",
+        },
+      ],
+      accomniments: [
+        {
+          isVeg: true,
+          name: "Raita",
+          price: 69,
+          details: "A delish side dish made with fresh curd and vegetables.",
+          imgLocation: "./images/dishes/raita.jpeg",
+        },
+      ],
     },
 
     cart: [],
@@ -54,15 +151,27 @@
       sidebarList.innerHTML = "";
     },
 
-    renderSidebarMenuItems: function (item) {
+    renderSidebarMenuItems: function (item, index, activeIndex) {
       const sidebarList = document.querySelector(".sidebar__list");
 
       let sidebarListItem = document.createElement("li");
       sidebarListItem.setAttribute("class", "sidebar__list__item");
-      //   if the menu is active use this class-> .sidebar__list__item--active
+      if (index === activeIndex) {
+        sidebarListItem.classList.add("sidebar__list__item--active");
+      }
       sidebarListItem.innerHTML = item;
       console.log(item);
       sidebarList.appendChild(sidebarListItem);
+    },
+
+    renderMenuHeading: function (activeIndex) {
+      const contentHeading = document.querySelector(".content__heading");
+      contentHeading.innerHTML = `
+        <h1 class="content__heading--big">${model.sidebarList[activeIndex]}</h1>
+        <p class="content__heading--small">${
+          model.sidebarListItems[model.sidebarList[activeIndex]].length
+        } ITEMS</p>
+        `;
     },
 
     renderMenuItems: function (item) {
@@ -100,11 +209,12 @@
       contentList.appendChild(contentListItem);
     },
 
-    render: function (sidebarList, menuItems) {
+    render: function (sidebarList, menuItems, activeIndex) {
       this.clearList();
 
+      this.renderMenuHeading(activeIndex);
       for (let i = 0; i < sidebarList.length; i++) {
-        this.renderSidebarMenuItems(sidebarList[i]);
+        this.renderSidebarMenuItems(sidebarList[i], i, activeIndex);
       }
 
       for (let i = 0; i < menuItems.length; i++) {
@@ -116,13 +226,47 @@
   //Controller
   controller = {
     init: function () {
-      this.getListItems("recommended");
+      const sidebarList = document.querySelector(".sidebar__list");
+      sidebarList.addEventListener("click", this.updateActiveIndex);
+      this.getListItems(model.activeMenuIndex);
     },
+    getIndex: function (element) {
+      const sidebarList = document.querySelector(".sidebar__list");
+      const len = sidebarList.childNodes.length;
+      for (let i = 0; i < len; i++) {
+        if (sidebarList.childNodes[i] === element) {
+          return i;
+        }
+      }
+      return 0;
+    },
+    updateActiveIndex: function (e) {
+      console.log(e.target);
+      const item = e.target;
+      const index = controller.getIndex(item);
 
-    getListItems: function (menuType) {
+      const updatedModel = controller.changeModel(
+        model,
+        model.actions.CHANGE_ACTIVE_MENU,
+        index
+      );
+      model = updatedModel;
+      controller.getListItems(index);
+    },
+    getListItems: function (activeMenuIndex) {
+      const activeIndex = activeMenuIndex;
       const sidebarList = model.sidebarList;
-      const menuItems = model.sidebarListItems[menuType];
-      view.render(sidebarList, menuItems);
+      const menuItems = model.sidebarListItems[model.sidebarList[activeIndex]];
+      view.render(sidebarList, menuItems, activeIndex);
+    },
+    changeModel: function (state, action, payload) {
+      switch (action) {
+        case model.actions.CHANGE_ACTIVE_MENU:
+          state.activeMenuIndex = payload;
+          return state;
+        default:
+          return state;
+      }
     },
   };
 
