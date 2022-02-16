59737104e6d11abb54ea4b1822926b858f5acac1
Prateek Ranjan <pranjan@tekion.com>
2022-02-17 00:28:08 +0530

    script.js file is created. Food Items display is now rendered dynamically and static html for that section is removed from index.html and food-item ADD button position is changed in styles.css.

---
+++ script.js
@@ -0,0 +1,104 @@
+//IIFE
+(function swiggyClone() {
+  //Model
+  model = {
+    sidebarList: {
+      recommended: [
+        {
+          isVeg: false,
+          name: "Premium Butter Chicken Roti Thali",
+          price: 289,
+          details:
+            "Butter Chicken , 2 nos roti served with pickle , raita ,gulab jamun, 2 Pcs Chicken Tikka",
+          imgLocation: "./images/dishes/premium-butter-chicken-roti-thali.jpeg",
+        },
+        {
+          isVeg: true,
+          name: "Veg Platter",
+          price: 699,
+          details:
+            "An authentic veg platter with 3 pieces of Paneer Achari, 3 pieces of Hara Bhara, 3 pieces of Veg Seekh and 3 pieces of Malai Chaap.",
+          imgLocation: "./images/dishes/veg-platter.jpeg",
+        },
+        {
+          isVeg: false,
+          name: "Non Veg Platter",
+          price: 899,
+          details:
+            "An authentic non veg platter with 3 pieces of Barnala Mathi Chicken Tikka, 3 pieces of Dhaniya Chicken, 3 pieces of Tawa Chicken and 3 pieces of Chicken Kebab.",
+          imgLocation: "./images/dishes/non-veg-platter.jpeg",
+        },
+      ],
+    },
+
+    cart: [],
+  };
+
+  //View
+  view = {
+    clearList: function () {
+      const contentList = document.querySelector(".content__list");
+      contentList.innerHTML = "";
+    },
+
+    renderMenuItems: function (item) {
+      const contentList = document.querySelector(".content__list");
+
+      const { isVeg, name, price, details, imgLocation } = item;
+
+      let contentListItem = document.createElement("li");
+      contentListItem.setAttribute("class", "content__list__item");
+      contentListItem.innerHTML = `
+        <div class="content__list__item--text">
+            ${
+              isVeg
+                ? '<p class="veg"><i class="fa-solid fa-circle-stop"></i></p>'
+                : '<p class="non-veg"><i class="fa-solid fa-square-caret-up"></i></p>'
+            }
+            <p class="list__item__dish-name">
+                ${name}
+            </p>
+            <p class="list__item__dish-price">&#x20B9; ${price}</p>
+            <p class="list__item__dish-items">
+                ${details}
+            </p>
+        </div>
+        <div class="content__list__item__img-container">
+            <img
+                class="list__item__dish-image"
+                src=${imgLocation}
+                alt="food-image"
+            />
+            <button class="list__item__dish-btn">ADD</button>
+        </div>
+        `;
+
+      contentList.appendChild(contentListItem);
+    },
+
+    render: function (menuItems) {
+      this.clearList();
+
+      for (let i = 0; i < menuItems.length; i++) {
+        this.renderMenuItems(menuItems[i]);
+      }
+    },
+  };
+
+  //Controller
+  controller = {
+    init: function () {
+      this.getListItems("recommended");
+    },
+
+    getListItems: function (menuType) {
+      const data = model.sidebarList[menuType];
+      console.log(data);
+      view.render(data);
+    },
+  };
+
+  window.onload = function () {
+    controller.init();
+  };
+})();
